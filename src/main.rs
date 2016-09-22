cb376d702335154661c8bb852f329c689a12dfa3
Michael Bohn <michael.bohn@freiheit.com>
2016-09-22 06:06:37 +0200

    read projects from server

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
 #[macro_use] extern crate hyper;
-extern crate rustc_serialize;
+extern crate serde;
+extern crate serde_json;
 
 use std::io::Read;
 use hyper::Client;
 use hyper::client::RequestBuilder;
-use rustc_serialize::json::Json;
-use std::collections::BTreeSet;
+use serde_json::Value;
 
 // TODO make token configurable via config file
-const AUTH_TOKEN : &'static str = "<insert-token-here>";
+const META_TOKEN : &'static str = "<todo>";
+const STAT_TOKEN : &'static str = "<todo>";
 
 header! { (AuthToken, "auth-token") => [String] }
 
 fn main() {
-    // Create a client.
     let client = Client::new();
+    let projects = get_projects(&client);
 
-    // Creating an outgoing request.
-    let mut res = client.get("https://130.211.118.12/meta/projects")
-        // set a header
-        .header(AuthToken(AUTH_TOKEN.to_owned()))
-        // let 'er go!
-        .send().unwrap();
+    println!("projects ({}) {:?}", projects.len(), projects);
 
-    // Read the Response.
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-
-    let response = Json::from_str(&body).unwrap();
-    let data = response.as_object().unwrap().get("projects").unwrap().as_array().unwrap();
-
-    let mut result = BTreeSet::new();
-    for project in data {
-        let name = project.as_object().unwrap().get("project").unwrap().as_string().unwrap();
-        result.insert(name);
-    }
-
-    println!("projects ({}) {:?}", result.len(), result);
+    // TODO use stat_token for this request to work!!!!!
+    println!("diff0 {:?}", get_diff_perc(&client, projects[0].as_str()));
 }
 
 fn get_request<'a>(client: &'a Client, resource: &'a str) -> RequestBuilder<'a> {
     let url : &str = &format!("{}{}", "https://130.211.118.12/", resource);
     let res = client.get(url)
-        .header(AuthToken(AUTH_TOKEN.to_owned()));
+        .header(AuthToken(META_TOKEN.to_owned()));
     return res;
 }
 
-fn get_projects<'a>(client: &'a Client) {
+fn get_diff_perc<'a>(client: &'a Client, proj: &'a str) -> f64 {
+    let url : &str = &format!("{}{}", "statistics/coverage/diff/", proj);
+    let req = get_request(client, url);
+    let mut response = req.send().unwrap();
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+
+    let json: Value = serde_json::from_str(&body).unwrap();
+    return json.as_object().unwrap().get("diff-percentage").unwrap().as_f64().unwrap();
+}
+
+fn get_projects<'a>(client: &'a Client) -> Vec<String> {
     let req = get_request(client, "meta/projects");
-    println!("result {:?}", req.send().unwrap());
+    let mut response = req.send().unwrap();
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+
+    let json: Value = serde_json::from_str(&body).unwrap();
+    let projects = json.as_object().unwrap().get("projects").unwrap().as_array().unwrap();
+
+    return projects.iter().map(|p| p.as_object().unwrap().get("project").unwrap().as_str().unwrap().to_string()).collect();
 }
