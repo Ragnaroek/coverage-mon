d65443889bb364ee3f13be3ff477b8f2628ce787
Michael Bohn <michael.bohn@freiheit.com>
2016-10-27 07:06:01 +0200

    build scripts now use release version

--- pi_scripts/build.sh
+++ pi_scripts/build.sh
@@ -1,1 +1,1 @@
-docker run --volume ~/_deps:/home/cross/deb-deps --volume ~/pprojects/coverage_mon:/home/cross/project ragnaroek/rust-raspberry:1.12.1 build
+docker run --volume ~/_deps:/home/cross/deb-deps --volume ~/pprojects/coverage_mon:/home/cross/project ragnaroek/rust-raspberry:1.12.1 build --release
